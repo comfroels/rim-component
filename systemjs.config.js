bcee151c663ea5a8ec4501ce57a9dd4f808564de
Nathan White <comfroels@gmail.com>
2016-12-01 11:00:08 -0700

    Beginning of webpack config

--- systemjs.config.js
+++ systemjs.config.js
@@ -33,8 +33,8 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: './main.js',
-        defaultExtension: 'js'
+        main: './main.ts',
+        defaultExtension: 'ts'
       },
       rxjs: {
         defaultExtension: 'js'
